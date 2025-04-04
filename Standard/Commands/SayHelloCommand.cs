2ff838bdbc08d99736dd55016b951bd5851ef25d
李锶厅(lisiting01) <lisiting01@longfor.com>
2025-04-05 02:45:57 +0800

    添加项目文件。

---
+++ Standard/Commands/SayHelloCommand.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.UI;
+using Newtonsoft.Json.Linq;
+using revit_mcp_sdk.API.Base;
+using revit_mcp_sdk.API.Models;
+
+namespace Standard.Commands
+{
+    public class SayHelloCommand : ExternalEventCommandBase
+    {
+        private SayHelloEventHandler _handler => (SayHelloEventHandler)Handler;
+
+        public override string CommandName => "say_hello";
+
+        public SayHelloCommand(UIApplication uiApp)
+        : base(new SayHelloEventHandler(), uiApp)
+        {
+        }
+
+        public override object Execute(JObject parameters, string requestId)
+        {
+            RaiseAndWaitForCompletion(15000);
+            return CommandResult.CreateSuccess(new { execute = true });
+        }
+    }
+}
